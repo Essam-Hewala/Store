a9ab2b2d66cf1d145a5b5572d69e06d489e59414
Essam Sayed <essamhewala84@gmail.com>
2023-02-03 16:16:16 +0200

    Add Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("helo");
+import express , { Application } from 'express';
+const port = 3150;
+const app:Application = express();
+app.listen(port,()=>{
+    console.log(`Server Runing On Port ${port}`);
+})
+export default app;
