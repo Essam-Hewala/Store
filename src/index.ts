c4299ea46c2092ad919149226d6b2eab357a573b
Essam Sayed <essamhewala84@gmail.com>
2023-02-04 16:13:13 +0200

    'Update'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import express , { Application } from 'express';
+import express , { Application ,Request,Response} from 'express';
 const port = 3150;
 const app:Application = express();
+app.get('/',(req:Request,res:Response)=>{
+    res.send("Hello");
+})
 app.listen(port,()=>{
     console.log(`Server Runing On Port ${port}`);
 })
---
+++ src/test/indexSpec.ts
@@ -0,0 +1,9 @@
+import supertest from "supertest";
+import app from "../index";
+const request = supertest(app);
+describe('Test Server Express',()=>{
+    it('Get The / Endpoint',async()=>{
+        const response = await request.get('/');
+        expect(response.status).toBe(200);
+    })
+})
