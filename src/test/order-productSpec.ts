e34e65a4b45953ba427337d52c308c254d6b9017
Essam Sayed <essamhewala84@gmail.com>
2023-02-11 22:25:54 +0200

    Update

--- src/test/order-productSpec.ts
+++ src/test/order-productSpec.ts
@@ -1,5 +1,12 @@
 import Orderproduct from "../models/order-prodcu.model";
+import UserModel from "../models/user.model";
+import Productmodel from "../models/product.model";
+import Ordermodel from "../models/order.model";
 const order = new Orderproduct();
+const userModel = new UserModel();
+const prod = new Productmodel();
+const ordermodel = new Ordermodel();
+
 describe("Order Model", () => {
   describe("Test CRUD Operations On Orders Product Model", () => {
     it("Should Retrun All Order Products", () => {
@@ -18,4 +25,33 @@ describe("Order Model", () => {
       expect(order.create).toBeDefined();
     });
   });
+  describe("Orders models Testing", () => {
+    beforeAll(async () => {
+      await userModel.create({
+        username: "Essam_Elsyed",
+        first_name: "Essam",
+        last_name: "Sayed",
+        password: "A_123456",
+      });
+      await prod.create({
+        name: "Test",
+        price: 35
+       
+      });
+    });
+    it("Test creating a new order", async () => {
+      const orders = {
+        user_id: "1",
+        product_id: "1",
+        quantity: "511",
+      };
+      const add = await order.create(orders);
+      expect(orders.quantity).toBe("511" as unknown as string)
+    });
+    it("Test listing orders", async () => {
+      const orders = await order.getall;
+      expect(orders.length).toBe(1);
+    });
+  });
+
 });
--- src/test/productSpec.ts
+++ src/test/productSpec.ts
@@ -1,4 +1,5 @@
 import Productmodel from "../models/product.model";
+import db from "../database/index"
 const productmodel = new Productmodel();
 describe("Products Model", () => {
   describe("Test CRUD Operations On Product Model", () => {
@@ -16,6 +17,27 @@ describe("Products Model", () => {
     });
     it("Should Create  Product", () => {
       expect(productmodel.create).toBeDefined();
+    });
+  });
+  describe("Products models Testing", () => {
+    const product = {
+      name: "Product",
+      price: 25,
+    };
+    it("Create a new product ", async () => {
+      const Prod = await productmodel.create(product);
+      expect(Prod.name).toBe("Product");
+      expect(Prod.price).toBe(25);
+    });
+
+    it("Get single product model", async () => {
+      const Prod = await productmodel.getone(1 as unknown as string);
+      expect(Prod.name).toBe("Product");
+    });
+
+    it("Testing list all products", async () => {
+      const Prod = await productmodel.getall;
+      expect(Prod.length).toBe(1);
     });
   });
 });
